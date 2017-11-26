85fa9c208c12fdd965696df17777edcd93a4cefd
DiGun <digun@mail.ru>
2017-11-26 15:18:37 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 26.11.2017 15:08:19
+ *  Author: DiGun
+ */ 
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
