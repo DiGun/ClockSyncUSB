7bb6a3c8c9ae8efa07b856f8c4340c1708da94d7
DiGun <digun@mail.ru>
2017-11-28 10:23:06 +0200

    +tm1637

--- main.c
+++ main.c
@@ -6,11 +6,26 @@
  */ 
 
 #include <avr/io.h>
+#include <util/delay.h>
+#include "tm1637.h"
 
 int main(void)
 {
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+uint8_t i = 0;
+
+/* setup */
+TM1637_init();
+
+/* loop */
+while (1) {
+	TM1637_display_digit(TM1637_SET_ADR_00H, i % 0x10);
+	TM1637_display_digit(TM1637_SET_ADR_01H, (i + 1) % 0x10);
+	TM1637_display_digit(TM1637_SET_ADR_02H, (i + 2) % 0x10);
+	TM1637_display_digit(TM1637_SET_ADR_03H, (i + 3) % 0x10);
+	TM1637_display_colon(true);
+	_delay_ms(200);
+	TM1637_display_colon(false);
+	_delay_ms(200);
+	i++;
+}
 }
