3d7de0ca729dc8d1840c86b97f21abc991f650d6
DiGun <digun@mail.ru>
2017-11-29 13:56:15 +0200

    + time + uart

--- main.c
+++ main.c
@@ -7,19 +7,31 @@
 
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
 #include "tm1637.h"
+#include "rtc.h"
+#include "uart.h"
 
 int main(void)
 {
   uint8_t i = 0;
+  uint8_t h,m,s;
+//  rtc_date date;
+//  struct rtc_time time;
 
 /* setup */
+  uart_init();
+  sei();
+// RTC configuration
+  twi_init_master();
+  rtc_init();
   TM1637_init();
 
 /* loop */
   while (1) 
   {
-	TM1637_setTime(i%99,(255-i)%99);
+	rtc_get_time_s(&h,&m,&s);
+	TM1637_setTime(m,s);
 	TM1637_display_colon(true);
 	_delay_ms(200);
 	TM1637_display_colon(false);
